58c87d566f93ee865a40349541ed67ff5b1c1221
Jdcdp <jaimedecastrop@gmail.com>
2019-04-25 11:15:34 +0200

    Renombrado main.c

---
+++ main.c
@@ -0,0 +1,16 @@
+#include "keyboard.h"
+#include "motion.h"
+#include "coin.h"
+
+void init(){
+// Aqui se llaman a las funciones de inicialización
+}
+
+
+int main(){
+	init();
+	HOLD(); /*Esto podria ser un comando para poner el procesador en modo ahorro de energia y */
+		/*funcionar solo con interrupciones*/
+return 0;
+}
+
