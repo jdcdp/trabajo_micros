5a79071e279b27b67d2e94c553954266a7c13062
GuillermoNR <49336757+GuillermoNR@users.noreply.github.com>
2019-04-25 23:01:48 +0200

    Add files via upload

--- drivers/time_m.c
+++ drivers/time_m.c
@@ -1,19 +1,46 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdint.h>
 #include "time_m.h"
 
-
+/*
 void init_timers(){
 	//TO DO
 	//Inicializar timer para delays de duracion F_CPU/1000000L
-}
+}*/
 
-void micros(){
-	//TO DO
-	//Interrupcion que sume 1 a micros_total
 
+
+int micros(){
+	return micros_total;
 }
 
 void delays(uint8_t ms){
+	
 	sei();
-	int end_time=micros()+ms*1000;
-	while (read_time_ms()<end_time)//change with an interruption and HOLD()
+	int end_time=micros()+ms*10;
+	while (micros()<end_time);//change with an interruption and HOLD()
+ 
 }
+
+
+
+
+ISR(TIMER0_COMPA_vect){
+	++micros_total;
+}
+
+void setup_time(){
+	cli();
+	
+	TIME_TCCRnA =0x02; //Modo CTC
+	
+	TIME_OCRnA =TIME_MAX; //Set timer TOP_A	
+
+	TIME_TCCRnB=0x01; //Preescalado de 0, cuenta cada us (1MHz)
+
+	TIMSK0 |= (1 << OCIE0A); //Enable Interrupt from CTC_A
+	
+	sei();
+}
+
--- drivers/time_m.h
+++ drivers/time_m.h
@@ -1,14 +1,24 @@
-#ifndef __TIME_M__
-#define __TIME_M__
-#include "time_m.c"
+#ifndef TIME_M_H_
+#define TIME_M_H_
+
+//#include "time_m.c"
 
 uint64_t micros_total=0;
 
-void init_timers();
+//void init_timers();
 
 void micros();
 
 void delay(uint8_t ms);
+
+void setup_timers();
+
+#define TIME_MAX 100 //0.1ms
+
+#define TIME_TCCRnA TCCR0A
+#define TIME_OCRnA OCR0A
+
+#define TIME_TCCRnB TCCR0B
 
 
 #endif
