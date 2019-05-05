96aa46be1fa3d6b5f7e20867961ddb2bbae3b199
eduuu18 <49990225+eduuu18@users.noreply.github.com>
2019-05-05 17:59:06 +0200

    Update barrera.h

--- barrera.h
+++ barrera.h
@@ -1,8 +1,10 @@
 #ifndef _BARRERA_H_
 #define _BARRERA_H_
 
-#include "drivers/motor.h"
+
 #include <stdint.h>
+#define SW9PIN	(PINB & 0x02)
+#define SW9PORT	(PORTB & 0x02)
 
 void abrir_puerta();
 
