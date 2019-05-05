a30ec10c0520ce8137f2194dc252e82cbf4973f3
eduuu18 <49990225+eduuu18@users.noreply.github.com>
2019-05-05 18:14:15 +0200

    Update barrera.h

--- barrera.h
+++ barrera.h
@@ -4,6 +4,12 @@
 
 #include <stdint.h>
 
+#define isSet( P, B )  		(P  & (1 << B))           //Consulta el estado del bit B, devuelve un 1 si el bit B es un 1
+#define isClr( P, B )  		(~P & (1 << B))           //Consulta el estado del bit B,devuelve un 1 si el bit B es un 0
+#define Bk_ON()		        setBit(PORTB,2)           //Activación de la señal M5_bk, frenado dinámico ON
+#define Bk_OFF()	        clrBit(PORTB,2)           //Activación de la señal M5_bk, frenado dinámico OFF
+
+
 //Microinterruptor SW9
 #define SW9PIN	(PINB & 0x02)
 #define SW9PORT	(PORTB & 0x02)
