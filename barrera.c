b37b3b09b40f44fa829b4ed9da985f64833b3c19
eduuu18 <49990225+eduuu18@users.noreply.github.com>
2019-05-06 12:57:31 +0200

    Update barrera.c

--- barrera.c
+++ barrera.c
@@ -9,10 +9,11 @@ void abrir_puerta(){
 //comprobar el estado de SW9, si esta en la posicion de cerrado, accionar m5
   if(isSet( P, B )){
       Bk_ON();
-        }
-  else{
-      Bk_OFF(); 
+        
+      if(isClr( P, B )){
+         Bk_OFF();
          }
+    }
 }
 
 
@@ -21,10 +22,11 @@ void cerrar_puerta(){
 //comprobar el estado de SW9, si esta en la posición de abierto, accionar m5
   if(isClr( P, B )){
       Bk_ON();
-         }
-  else{
-      Bk_OFF(); 
-         }
+         
+      if(isSet( P, B )){
+          Bk_OFF();
+          }
+        }
 }
 
 //Conviene manejar el motor m5 con la señal de frenado dinamico, para parar la barrera en su lugar exacto
