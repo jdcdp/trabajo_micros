5c5b03f33aefe96b2ef82f2aa302bd558ac98b2a
GuillermoNR <49336757+GuillermoNR@users.noreply.github.com>
2019-05-07 19:24:10 +0200

    Update time.h

--- drivers/time.h
+++ drivers/time.h
@@ -4,8 +4,15 @@
 #include "avr/io.h"
 #include "../pinout.h"
 
+//Registros Timer 0
 #define TIME_MAX 100 //0.1ms
 
+#define TIME_TCCRnA TCCR0A
+#define TIME_OCRnA OCR0A
+
+#define TIME_TCCRnB TCCR0B
+
+//Variables y Funciones
 uint64_t current_time;
 
 void init_time();
