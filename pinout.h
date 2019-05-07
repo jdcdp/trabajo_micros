feaed8c99e1f688d7bcae74fb1980a9acfa6deef
Jdcdp <jaimedecastrop@gmail.com>
2019-05-07 17:59:25 +0200

    Changed pinout

--- pinout.h
+++ pinout.h
@@ -3,7 +3,7 @@
 #define _PINOUT_H_
 
 //Sensor Optico 1
-#define S01_PIN	(PINL & 0x01)
+#define S01_PIN		(PINL & 0x01)
 #define S01_PORT	(PORTL & 0x01)
 
 //Timer con Input Capture
@@ -16,14 +16,14 @@
 #define S01_TIMERn_CAPT_vect TIMER4_CAPT_vect
 
 //Sensor Optico 2
-#define S02_PIN	(PINL & 0x02)
+#define S02_PIN		(PINL & 0x02)
 #define S02_PORT	(PORTL & 0x02)
 
 //Timer con Input Capture
-#define S02_TCCRnB	TCCR5B
-#define S02_TCNTn 	TCNT5
-#define S02_TIMSKn 	TIMSK5
-#define S02_ICRn 	ICR5
+#define S02_TCCRnB	TCCR1B
+#define S02_TCNTn 	TCNT1
+#define S02_TIMSKn 	TIMSK1
+#define S02_ICRn 	ICR1
 
 
 //drivers/time_m.h
@@ -32,23 +32,23 @@
 #define TIME_TCCRB	TCCR0B
 
 //drivers/pwm.h
-#define PWM_TCCRA 	TCCR1A
-#define PWM_TCCRB 	TCCR1B
-#define PWM_TCCRC 	TCCR1C
-#define PWM_OCR_M1  	OCR1A
-#define PWM_OCR_M2  	OCR1B
-#define PWM_OCR_M3  	OCR1A
-#define PWM_TOP     	ICR1
-#define PWM_DDR		DDRB
+#define PWM_TCCRA 	TCCR5A
+#define PWM_TCCRB 	TCCR5B
+#define PWM_TCCRC 	TCCR5C
+#define PWM_OCR_M1  	OCR5A
+#define PWM_OCR_M2  	OCR5B
+#define PWM_OCR_M3  	OCR5C
+#define PWM_TOP     	ICR5
+#define PWM_DDR		DDRL
 
 //drivers/motor.h
 #define ENDSTOPS	        PINK
-#define ENDSTOPDDR	      DDRK  	
-#define ENSTOP_INTERRUPT  PCINT1_vect
-#define MOTOR_DIR_PORT	  PORTK
-#define M1_DIR		        2
-#define M2_DIR            6
-#define M3_DIR            7
+#define ENDSTOPDDR	      	DDRK
+#define ENSTOP_INTERRUPT  	PCINT1_vect
+#define MOTOR_DIR_PORT	  	PORTK
+#define M1_DIR		        3
+#define M2_DIR            	4
+#define M3_DIR            	5
 
 
 
