f0bfd7480a6b9f613b67f422e623fc8a06e9ef54
jdcdp <47483528+jdcdp@users.noreply.github.com>
2019-05-20 11:18:09 +0200

    Update time.h

--- drivers/time.h
+++ drivers/time.h
@@ -5,6 +5,7 @@
 #include "../pinout.h"
 
 //Registros Timer 0
+
 #define TIME_MAX  100 //0.1ms
 
 #define TIME_TCCRA TCCR0A
@@ -23,5 +24,31 @@ uint64_t timenow();
 void delay(uint16_t ms);
 
 ISR(TIMER0_COMPA_vect);
+
+
+//Registros Timer Barrera
+
+//Max escalado 8 = 65.536ms //1000= 1ms
+//Max escalado 64 = 0.524s //125=1ms  
+
+#define BARRERA_ESPERA 12500 //0.1s
+
+#define BARRERA_TCCRB TCCR3B
+
+#define BARRERA_OCRA  OCR3A
+
+#define BARRERA_CNT	TCNT3
+
+#define BARRERA_TIMSK  TIMSK3
+
+
+//Variables y Funciones
+
+void time_init();
+void esperar_moneda();
+
+ISR(TIMER3_COMPA_vect);
+ISR(TIMER3_COMPB_vect);
+
 
 #endif
