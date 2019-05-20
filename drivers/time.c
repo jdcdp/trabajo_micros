41665884640a6bb15962c29536beec7d44386b87
GuillermoNR <49336757+GuillermoNR@users.noreply.github.com>
2019-05-20 19:30:06 +0200

    Update time.c

--- drivers/time.c
+++ drivers/time.c
@@ -17,13 +17,10 @@ void time_init(){
         TIME_TIMSK |= (1 << OCIE0A); //Enable Interrupt from CTC_A
 	
 	////////Barrera:
-	//Modo CTC 
-	BARRERA_TCCRB |= (1<<WGM32);
-
-	//Tiempos necesarios de espera
-	BARRERA_OCRA = BARRERA_ESPERA; //Set timer TOP_A; 8000 es 1ms
 	
-
+	BARRERA_TCCRB |= (1<<WGM32); //Modo CTC 
+	BARRERA_TCCRB |= (1<<CS31)|(1<<CS30); //Con preescalado 64, cuenta cada 8us (8MHz)
+	
         sei();
 }
 
@@ -47,8 +44,10 @@ ISR(TIMER0_COMPA_vect){
 
 void esperar_moneda(){ //Espera a que pase la moneda para cerrar la barrera
 	
+	//Tiempos necesarios de espera
+	//MIRAR SI ES UNISiGNED INT O NO PARA PONER TIPO EN LA OPERACION
+	BARRERA_OCRA = BARRERA_CNT + BARRERA_ESPERA ; //Set timer TOP_A;
 	BARRERA_TIMSK |= (1 << OCIE3A); //Poner interrupcion de CTC_A
-	BARRERA_TCCRB |= (1<<CS31)|(1<<CS30); //Con preescalado 64, cuenta cada 8us (8MHz) (Mirar problemas Monedero)
 	
 	//PORTB= ~PINB; //PRUEBAS
 }
@@ -58,11 +57,14 @@ ISR(TIMER3_COMPA_vect){ //Solo para esperar a la moneda
 	//Quitar interrupcion de CTC_A
 	BARRERA_TIMSK &= ~(1 << OCIE3A);
 		
-	cerrar_barrera();	
+	cerrar_barrera();
+	
 	//PORTB= ~PINB; //PRUEBAS
-
+	
+	/*
 	//Parar y poner a 0 timer
 	BARRERA_TCCRB &= ~((1<<CS31)|(1<<CS30));
 	BARRERA_CNT= 0x00;
+	*/
 
 }
