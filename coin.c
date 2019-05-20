1a10c229873126c23a03c3a6b00ebe7fe8b1434e
jdcdp <47483528+jdcdp@users.noreply.github.com>
2019-05-20 11:03:27 +0200

    Add files via upload

--- coin.c
+++ coin.c
@@ -1,29 +1,76 @@
 /*
+ * coin.c
+ *
+ * Created: 08/05/2019 20:49:11
+ * Author : Guillermo
+ */ 
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#include <stdint.h>
 
 #include "coin.h"
-#include "pinout.h"
+#include "time.h"
 
-uint16_t ta,tb;
+uint16_t ta,tb,ta1,tb1,ta2,tb2;
 float relacion;
 
-uint8_t ultima_moneda;
+int8_t ultima_moneda;
 float saldo;
 
+uint8_t aligned;
+
+void syscall_aligned(){
+	
+	aligned=1;
+	check_saldo();
+	
+}
+
+void substract_saldo(uint8_t gasto){
+	
+	saldo-= gasto;
+	
+}
 
 float get_saldo(){
 	return saldo;
 }
 
+
 int get_ultima_moneda(){
 	return ultima_moneda;
 }
 
-//Funciones Internas
+//Funciones Internas---------------------------------------------------
 
-int calculate_ultima_moneda(float relacion){
+void check_saldo(){
+	
+	if(saldo > 1.20){
+		moveY();
+		aligned=0;
+	}
+	
+}
+
+void abrir_barrera(){
+	
+	//PROBAR MACRO SW9_PIN
+	if( (PIND & 0b00000100) != 0){ //Barrera Cerrada es 1
+		PORTD &= ~(1<<PD5); //Encender M5
+	}
+	
+}
+
+void cerrar_barrera(){
+	
+	if( (PIND & 0b00000100) == 0){ //Barrera Abierta es 0 
+		PORTD &= ~(1<<PD5); //Encender M5
+	}
+	
+}
+
+
+uint8_t calculate_ultima_moneda(float relacion){
 
 	if((relacion > C10MIN) && (relacion < C10MAX)){
 		return 1; //10 cent
@@ -37,14 +84,14 @@ int calculate_ultima_moneda(float relacion){
 	else if((relacion > E1MIN) && (relacion < E1MAX)){
 		return 4; //1 euro
 	}
-	else {
-		return 0; //No reconocida
+	else{
+		return -1; //No reconocida
 	}
 }
 
 void add_saldo(int ultima_moneda){
 	
-	if(ultima_moneda!=0){
+	if(ultima_moneda!= -1){
 
 		if(ultima_moneda==1){
 			saldo+=0.10;
@@ -62,51 +109,96 @@ void add_saldo(int ultima_moneda){
 	}
 }
 
-//Sensor Optico 1
+//Interruptor Barrera (SW9)
+ISR(INT2_vect){
+
+	//Frenar Motor
+	PORTD |= 1<<PD5;
+		
+}
+
+//Sensor Optico 1 (SO1)
 ISR(S01_TIMERn_CAPT_vect){
 
 	if((S01_PIN) != 0){ //Flanco Subida
-
-		//Arrancar Contadores con Preescalado de 8 (Bit1=1) y...
-		S01_TCCRnB=0x02; //Hacer que capture con flanco de bajada de S01 (Bit6=0)
-		S02_TCCRnB=0x42; //Flanco subida de S02 (Bit6=1)
+		
+		//Hacer que capture con flanco de bajada de S01 (Bit6=0)
+		S01_TCCRnB&= (~(1<<ICES4));
+		
+		//Cojo primer punto del tiempo
+		ta1=(uint16_t)S01_ICRn;
+		tb1=(uint16_t)S01_ICRn;
+	
 	}
 
 	if((S01_PIN) == 0){ //Flanco Bajada
+		
+		S01_TCCRnB|= 1<<ICES4; //Hacer que capture con flanco de subida de S01 (Bit6=1)
+		
+		ta2=(uint16_t)S01_ICRn;
+		tb2=(uint16_t)S02_ICRn;
 
-		//Determinacion tipo de moneda y suma del saldo
-		ta=(uint16_t)S01_ICRn;
-		tb=(uint16_t)S02_ICRn; //Estoy suponiendo que el SO2 siempre se activa antes que se desactiva el S01
-
+		ta=ta2-ta1;
+		tb=tb2-tb1;
+		
 		relacion=(float)ta/tb;
 		ultima_moneda= calculate_ultima_moneda(relacion);
+		
+		//Mover Barrera
+		if(ultima_moneda != -1){
+			abrir_barrera();
+			esperar_moneda();
+		}	
+		
 		add_saldo(ultima_moneda);
-
-		//Parar timers y ...
-		S01_TCCRnB=0x40; //Hacer que capture con flanco de subida de S01 (Bit6=1)
-		S02_TCCRnB=0x40; //Hacer que capture por flanco de subida de S02 (Bit6=1)
-
-		//Resetear Timers
-		S01_TCNTn=0x00;
-		S02_TCNTn=0x00;
+		check_saldo();
+		
 	}
 }
 
-void setup_coin(){
-	//Configuracion I/O (Para el setup del main)
-	//DDRL&=0b11111100; Bits 0 y 1 entradas
 
-	//Timers para Input Capture
+void setup_coin(){
+
 	cli();
 
-	S01_TIMSKn=0x20; //Activar rutina interrupcion por input capture
+	//Monedero-------------------------------------------------
+	//Timers para Input Capture
+	
+	S01_TIMSKn= 1<<ICIE4; //Activar rutina interrupcion por input capture
 	S02_TIMSKn=0x00; //No necesita rutina de interrupcion
 
-	S01_TCCRnB=0x40; //Hacer que capture sea por flanco de subida de S01 (Bit6=1)
-	setBit(S01_TCCRnB,7); //Antiruido
+	S01_TCCRnB= 1<<ICES4; //Hacer que capture sea por flanco de subida de S01 (Bit6=1)
+	S01_TCCRnB|= 1<<ICNC4; //Antirruido
 
-	S02_TCCRnB=0x40; //Hacer que capture sea por flanco de subida de S02 (Bit6=1)
-	setBit(S02_TCCRnB,7); //Antiruido
+	S02_TCCRnB= 1<<ICES1;	//Hacer que capture sea por flanco de subida de S02 (Bit6=1)
+	S02_TCCRnB|= 1<<ICNC1;  //Antirruido
+	
+	//Arrancar Contadores con Preescalado de 8 (Bit1=1)
+	S01_TCCRnB|= 1<<CS41;
+	S02_TCCRnB|= 1<<CS11;
+
+
+    //Barrera---------------------------------------------------
+	DDRD|= 1<<DDD5; // D5 salida
+	DDRD&= (~(1<<DDD2)); //D2 entrada
+	PORTD|= 1 << PD5; //Inicializo con freno activado
+	
+	EICRA= 1 << ISC20; //Interrupcion INT2 se activa por cualquier flanco 
+	EIMSK= 1 << INT2;  //Activar INT2
+
 	sei();
+	
+	esperar_moneda(); //Asegurar posicion inicial barrera: espera y cierra
+	
 }
-*/
+
+
+int main(void)
+{
+	setup_coin();
+
+	while (1){
+		
+	}
+}
+
--- coin.h
+++ coin.h
@@ -1,22 +1,84 @@
-#ifndef _COIN_H_
-#define _COIN_H_
+/*
+ * coin.h
+ *
+ * Created: 08/05/2019 20:50:22
+ *  Author: Guillermo
+ */ 
+
+
+#ifndef COIN_H_
+#define COIN_H_
+
+
+/*Monedero*/
 
 //Rangos Monedas
 #define C10MIN	1.16
 #define C10MAX	1.20
 
-#define C20MIN	1.33
+#define C20MIN	1.32
 #define C20MAX 	1.38
 
-#define C50MIN	1.44
+#define C50MIN	1.43
 #define C50MAX 	1.47
 
-#define E1MIN	1.395
+#define E1MIN	1.38
 #define E1MAX 	1.42
 
 //Posibles Funciones, seguramente haya que ponerlas en el general
 #define setBit(P, B)	(P |= (1 << B))
-#define resetBit(P,B)	(P &= ( ~(1 << B) ))
+#define resetBit(P,B) (P &= ( ~(1 << B) ))
 
 
-#endif
+//Sensor Optico 1
+#define S01_PIN		(PINL & 0x01)
+
+//Timer con Input Capture
+#define S01_TCCRnB	TCCR4B
+#define S01_TCNTn	TCNT4
+#define S01_TIMSKn 	TIMSK4
+#define S01_ICRn 	ICR4
+
+//Salto a Rutina
+#define S01_TIMERn_CAPT_vect TIMER4_CAPT_vect
+
+//Sensor Optico 2
+#define S02_PIN		(PIND & 0x10)
+
+//Timer con Input Capture
+#define S02_TCCRnB	TCCR1B
+#define S02_TCNTn 	TCNT1
+#define S02_TIMSKn 	TIMSK1
+#define S02_ICRn ICR1
+
+
+//Funciones
+
+float get_saldo();
+int get_ultima_moneda();
+
+int calculate_ultima_moneda(float relacion);
+void add_saldo(int ultima_moneda);
+
+
+ISR(S01_TIMERn_CAPT_vect);
+
+void setup_coin();
+
+/*Barrera*/
+
+//M5
+#define M5_BK_PIN	(PIND & 0x10)
+
+//SW9
+#define SW9_PIN		(PIND & 0x04)
+
+//Funciones
+
+ISR(INT2_vect);
+
+void abrir_barrera();
+void cerrar_barrera();
+
+
+#endif /* COIN_H_ */
